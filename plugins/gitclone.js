300cc746e466d548888e1845257424e158c73304
PRINCETECH19 <princerwanda10@gmail.com>
2025-06-01 09:53:07 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -55,8 +55,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363305273910720@newsletter',
-          newsletterName: '𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈-𝐓𝐄𝐂𝐇 🐯',
+          newsletterJid: '120363398106360290@newsletter',
+          newsletterName: 'PRINCE XMD',
           serverMessageId: 143
         }
       }
