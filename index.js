5009521bc462e134b34adaf442a9f89193c03f36
PRINCE TECH <majidirwanda815@gmail.com>
2025-06-01 06:30:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const {
   const path = require('path')
   const prefix = config.PREFIX
   
-  const ownerNumber = ['255612130873']
+  const ownerNumber = ['255697929318']
   
   const tempDir = path.join(os.tmpdir(), 'cache-temp')
   if (!fs.existsSync(tempDir)) {
@@ -201,7 +201,7 @@ const port = process.env.PORT || 9090;
   conn.sendMessage(from, { text: teks }, { quoted: mek })
   }
   const udp = botNumber.split('@')[0];
-    const jawad = ('255711765335', '255615944741', '255612130873');
+    const jawad = ('255697929318', '255697929318', '255697929318');
     let isCreator = [udp, jawad, config.DEV]
 					.map(v => v.replace(/[^0-9]/g) + '@s.whatsapp.net')
 					.includes(mek.sender);
