1c24d9aeefcf5cf15084ffb9b147120bb6d225a6
PRINCE XMD <princerwanda10@gmail.com>
2025-06-07 18:23:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255697929318",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255623672733",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "PRINCE-TECH",
 // add bot owner name
@@ -45,7 +45,7 @@ DESCRIPTION: process.env.DESCRIPTION || "*POWERED BY PRINCE TECH*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/15vem5.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> PRINCE XMD-𝐁𝐎𝐓 IS ALIVE 🐯",
+LIVE_MSG: process.env.LIVE_MSG || "> PRINCE XMD-𝐁𝐎𝐓 IS ALIVE 🇹🇿",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255697929318",
+DEV: process.env.DEV || "255623672733",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
